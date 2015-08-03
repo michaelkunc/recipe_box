2b54c2f70a93aeff40c2e272d77748f14d607943
michaelkunc <mjkunc@gmail.com>
2015-08-03 08:16:12 -0500

    start of nested forms

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 class Recipe < ActiveRecord::Base
+  has_many :ingredients
+  has_many :directions
+
+  accepts_nested_attributes_for :ingredients, :directions
+
+  validates :title, :description, :image, presence: true
   has_attached_file :image, styles: { medium: "400x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
